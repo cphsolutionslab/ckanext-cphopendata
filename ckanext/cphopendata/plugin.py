f7029a08aa6f92640d5c37013215f46cfba329f8
root <root@ckan-HP-ProBook-6560b.(none)>
2015-02-17 10:25:12 +0100

    Added files to rep

---
+++ ckanext/cphopendata/plugin.py
@@ -0,0 +1,38 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+def latest_datasets():
+    '''Return a sorted list of the latest datasets.'''
+
+    datasets = toolkit.get_action('package_search')(
+        data_dict={'rows': 10, 'sort': 'metadata_created desc' })
+
+    return datasets['results']
+
+
+def most_popular_datasets():
+    '''Return a sorted list of the most popular datasets.'''
+
+    datasets = toolkit.get_action('package_search')(
+        data_dict={'rows': 10, 'sort': 'views_recent desc' })
+
+    return datasets['results']
+
+
+class CPHOpenDataPlugin(plugins.SingletonPlugin):
+    '''cph.open-data.dk theme plugin.
+
+    '''
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
+
+    def update_config(self, config):
+        toolkit.add_template_directory(config, 'templates')
+        toolkit.add_public_directory(config, 'public')
+        toolkit.add_resource('fanstatic', 'cphopendata')
+
+    def get_helpers(self):
+        return {'cphopendata_latest_datasets': latest_datasets,
+                'cphopendata_most_popular_datasets': most_popular_datasets}
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+    name='ckanext-cphopendata',
+    version=version,
+    description="Theme for cph.open-data.dk",
+    long_description='''
+    ''',
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Henrik Aagaard Jorgensen',
+    author_email='haj@cphsolutionslab.dk',
+    url='http://cph.open-data.dk',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.cphopendata'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points='''
+        [ckan.plugins]
+        # Add plugins here, e.g.
+        cphopendata=ckanext.cphopendata.plugin:CPHOpenDataPlugin
+    ''',
+)
